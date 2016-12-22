a6138b0760f4eaa1bc6c14d54c788d25b84d4934
Raphael Zschorsch <zschorsch@werdewelt.info>
2016-12-22 14:22:44 +0100

    Pagekit

---
+++ app/templates/pagekit/config.php
@@ -0,0 +1,25 @@
+<?php return array (
+  'application' => 
+  array (
+    'debug' => false,
+  ),
+  'database' => 
+  array (
+    'default' => 'mysql',
+    'connections' => 
+    array (
+      'mysql' => 
+      array (
+        'host' => 'mysql',
+        'user' => 'root',
+        'password' => 'install',
+        'dbname' => 'pagekit-yeoman',
+        'prefix' => 'pk_',
+      ),
+    ),
+  ),
+  'system' => 
+  array (
+    'secret' => 'eYF2f0.ObQD7gdP4Hss6/4KoAiACj39SQwcP/vMKEmaWEsNnS.Hc3lN6sy.pzIOx',
+  ),
+);
